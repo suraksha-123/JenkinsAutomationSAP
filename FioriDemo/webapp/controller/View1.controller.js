38b4b15211a65f062788f5a511bf64ae7bee9fa7
Anshul Sharma <anshul.sharma@yash.com>
2020-12-03 17:04:52 +0530

    First commit

---
+++ FioriDemo/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("mynamespace.FioriDemo.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
